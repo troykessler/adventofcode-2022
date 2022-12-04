ab19a92c4bba66799b8255a4fef38e6860e61d84
Troy Kessler <troy.kessler99@gmail.com>
2022-12-04 12:58:00 +0100

    some refactorings

--- 4/part_1.py
+++ 4/part_1.py
@@ -1,5 +1,5 @@
 content = str(open("./4/inputs.txt", "r").read())
 
 p = [[[int(z) for z in y.split("-")] for y in x.split(",")] for x in content.split("\n")]
-s = [1 if (g[0][0] >= g[1][0] and g[0][1] <= g[1][1]) or (g[1][0] >= g[0][0] and g[1][1] <= g[0][1]) else 0 for g in p]
-print(sum(s))
+s = sum([1 if (x0[0] >= x1[0] and x0[1] <= x1[1]) or (x1[0] >= x0[0] and x1[1] <= x0[1]) else 0 for [x0, x1] in p])
+print(s)
--- 4/part_2.py
+++ 4/part_2.py
@@ -1,6 +1,6 @@
 content = str(open("./4/inputs.txt", "r").read())
 
 p = [[[int(z) for z in y.split("-")] for y in x.split(",")] for x in content.split("\n")]
-y = lambda e1, e2: e2[0] <= e1[0] <= e2[1] or e2[0] <= e1[1] <= e2[1]
-s = sum([1 if y(g[0], g[1]) or y(g[1], g[0]) else 0 for g in p])
+y = lambda x1, x2: x2[0] <= x1[0] <= x2[1] or x2[0] <= x1[1] <= x2[1]
+s = sum([1 if y(x0, x1) or y(x1, x0) else 0 for [x0, x1] in p])
 print(s)
