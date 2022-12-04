d8e971cc691ddb6ddef8889d3d8dc30f81d171c4
Troy Kessler <troy.kessler99@gmail.com>
2022-12-04 12:47:26 +0100

    refactored day 4 part 2

--- 4/part_2.py
+++ 4/part_2.py
@@ -1,16 +1,6 @@
 content = str(open("./4/inputs.txt", "r").read())
 
-score = 0
-
-for c in content:
-    pairs = list(map(lambda p: list(map(lambda i: int(i), p.split("-"))), c.split(",")))
-
-    if pairs[1][0] <= pairs[0][0] <= pairs[1][1] or pairs[1][0] <= pairs[0][1] <= pairs[1][1]:
-        score += 1
-        continue
-
-    if pairs[0][0] <= pairs[1][0] <= pairs[0][1] or pairs[0][0] <= pairs[1][1] <= pairs[0][1]:
-        score += 1
-        continue
-
-print(score)
+p = [[[int(z) for z in y.split("-")] for y in x.split(",")] for x in content.split("\n")]
+y = lambda e1, e2: e2[0] <= e1[0] <= e2[1] or e2[0] <= e1[1] <= e2[1]
+s = [1 if y(g[0], g[1]) or y(g[1], g[0]) else 0 for g in p]
+print(sum(s))
